fb8db99fc10b57624be60a837fa25a42c8907787
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-03-28 00:03:55 +0300

    added content form

--- src/pages/Add/Add.tsx
+++ src/pages/Add/Add.tsx
@@ -1,5 +1,11 @@
+import ContentForm from "../../components/ContentForm/ContentForm";
+
 const Add = () => {
-  return <div>Add</div>;
+  return (
+    <div>
+      <ContentForm />
+    </div>
+  );
 };
 
 export default Add;
