603ece4e4817e3436062976ba6b6c688d5137f59
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-03-27 21:41:40 +0300

    added toaster, context, query

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,25 @@
 import "./index.scss";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import { UserProvider } from "./contexts/UserContext";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <UserProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <Toaster />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </UserProvider>
   </React.StrictMode>
 );
