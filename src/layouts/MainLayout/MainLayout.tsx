c84a54884e6fd9f231a6e66ca5ce15879c5b519b
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-03-27 23:09:11 +0300

    maintopbar created

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,8 +1,10 @@
+import MainTopbar from "../../components/MainTopBar/MainTopBar";
 import { PropsWithChildren } from "react";
 
 const MainLayout = ({ children }: PropsWithChildren) => {
   return (
     <div>
+      <MainTopbar />
       <main>{children}</main>
     </div>
   );
