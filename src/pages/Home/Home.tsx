a764eb2a6fb6d6ed1a4b17a92a65bd223b5c796e
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-03-27 21:43:22 +0300

    reached user

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,10 @@
+import { UserContext } from "../../contexts/UserContext";
+import { useContext } from "react";
+
 const Home = () => {
+  const { setUser, setToken, user, token } = useContext(UserContext);
+  console.log(token);
+  console.log(user);
   return <div>Home</div>;
 };
 
