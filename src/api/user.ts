fd3c85e8604dfed83a6de4def3ce6d90ce8cd68d
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-03-27 21:41:58 +0300

    updated env

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,7 +1,7 @@
 import { User } from "../types/user";
 import axios from "axios";
 
-const BASE_URL = process.env.BASE_URL;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const registerUser = async (user: User): Promise<User> => {
   const response = await axios.post(`${BASE_URL}/v1/auth/register`, user);
