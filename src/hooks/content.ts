f021c98bc1f8262f10088648c520c85ee1cc25da
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-03-28 00:03:48 +0300

    updated query

--- src/hooks/content.ts
+++ src/hooks/content.ts
@@ -1,8 +1,17 @@
-import { fetchContent } from "../api/content";
-import { useQuery } from "@tanstack/react-query";
+import { createContent, fetchContent } from "../api/content";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 const CONTENT = "CONTENT";
 
 export const useContent = (token: string) => {
   return useQuery([CONTENT], () => fetchContent(token));
+};
+
+export const useCreateContent = () => {
+  return useMutation(
+    (variables: {
+      token: string;
+      content: { title: string; description: string };
+    }) => createContent(variables.token, variables.content)
+  );
 };
