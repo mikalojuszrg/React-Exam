644d6c93f631b748cc9578c9b31b70451ff84a2c
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-03-27 23:09:32 +0300

    home page updated

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,21 @@
+import { Content } from "../../types/content";
+import ContentCard from "../../components/ContentCard/ContentCard";
 import { UserContext } from "../../contexts/UserContext";
+import { useContent } from "../../hooks/content";
 import { useContext } from "react";
 
 const Home = () => {
-  const { setUser, setToken, user, token } = useContext(UserContext);
-  console.log(token);
-  console.log(user);
-  return <div>Home</div>;
+  const { token } = useContext(UserContext);
+  const { data } = useContent(token ? token : "");
+  const content = data || [];
+  console.log(content);
+  return (
+    <div>
+      {content.map(({ id, title, description }: Content) => (
+        <ContentCard key={id} title={title} description={description} />
+      ))}
+    </div>
+  );
 };
 
 export default Home;
