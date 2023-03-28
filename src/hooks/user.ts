f504a32dfdb70e9a1d3a696bebfa65444037ddbd
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-03-28 18:25:28 +0300

    deleted unecessary lines

--- src/hooks/user.ts
+++ src/hooks/user.ts
@@ -2,8 +2,6 @@ import { loginUser, registerUser } from "../api/user";
 
 import { useMutation } from "@tanstack/react-query";
 
-const USERS = "USERS";
-
 export const useRegisterUser = () => {
   return useMutation(registerUser);
 };
