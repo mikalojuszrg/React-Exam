a264bd5335821de8c9909c2af9569ff2f68444b1
“mikalojuszrg” <mikalojus.zirgulis@gmail.com>
2023-03-28 16:07:10 +0300

    added check if content exists

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,25 @@
 import { Content } from "../../types/content";
 import ContentCard from "../../components/ContentCard/ContentCard";
+import Loader from "../../components/Loader/Loader";
 import { UserContext } from "../../contexts/UserContext";
 import { useContent } from "../../hooks/content";
 import { useContext } from "react";
 
 const Home = () => {
   const { token } = useContext(UserContext);
-  const { data } = useContent(token ? token : "");
-  const content = data || [];
-  console.log(content);
+  const { data, isLoading } = useContent(token ? token : "");
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (!data || data.length === 0) {
+    return <div>No content posted.</div>;
+  }
+
   return (
     <div>
-      {content.map(({ id, title, description }: Content) => (
+      {data.map(({ id, title, description }: Content) => (
         <ContentCard key={id} title={title} description={description} />
       ))}
     </div>
